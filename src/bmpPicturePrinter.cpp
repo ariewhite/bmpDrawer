a492373a4e1cccc79f5754fc6f101072d59a3e53
Chingiz <arie.white@bk.ru>
2024-09-17 23:30:46 +0300

    input path from argv

--- src/bmpPicturePrinter.cpp
+++ src/bmpPicturePrinter.cpp
@@ -5,10 +5,28 @@
 
 using namespace std;
 
-int main()
-{
-	std::string path = "C:\\Users\\ariew\\project\\bmpPicturePrinter\\assets\\picture1.bmp";
+int main(int argc, char** argv)
+{	
+	std::setlocale(LC_ALL, "rus");
+	
 
+	if (argc < 2)
+	{
+		std::cerr << "Usage: " << argv[0] << " <path_to_bmp_file>\n"
+			<< "Example: bmpDrawer.exe C:\\User\\Image\\picture.bmp\n";
+		return 1;
+	}
+	std::string path = argv[1];
+
+	for (size_t i = 0; i < path.length(); ++i)
+	{
+		if (path[i] == '\\')
+		{
+			path.insert(i, "\\");
+			i++;
+		}
+	}
+	
 	BmpDrawer b;
 	b.openBMP(path);
 	
